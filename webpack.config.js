9af53222cae69f8f7c6ed6ea1842db318f861938
IronHeart7334 <greengrappler12@gmail.com>
2019-01-18 15:11:56 -0800

    The big one

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./wayfindingJSFiles/webpages/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
+  }
+}
