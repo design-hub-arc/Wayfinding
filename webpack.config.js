8b48817c746e6da95871fa9fa495935e6d3491a2
IronHeart7334 <greengrappler12@gmail.com>
2019-06-08 06:45:56 -0700

    I could have sworn I'd already fixed that

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    index: "./src/webpages/index.js",
-    classFinder: "./src/webpages/classFinder.js"
+    index: "./src/webpages/index.js"
   },
   output: {
     path: path.resolve(__dirname, "dist/src/webpages"),
